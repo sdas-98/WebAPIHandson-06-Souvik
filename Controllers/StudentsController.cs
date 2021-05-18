0d80b364c401c235cd5b9174e1c8a14ff967c1be
sdas <84001474+sdas-98@users.noreply.github.com>
2021-05-18 21:52:42 +0530

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentService.Models;
+
+namespace StudentService.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public HttpResponseMessage Put(int id,[FromBody]Student student)
+        {
+            try
+            {
+                using (StudentDBContext dbContext = new StudentDBContext())
+                {
+                    var entity = dbContext.Students.FirstOrDefault(s => s.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,
+                            "Student with Id" + id.ToString() + " not found to update");
+                    }
+                    else
+                    {
+                        entity.FirstName = student.FirstName;
+                        entity.LastName = student.LastName;
+                        entity.Gender = student.Gender;
+                        entity.Address = student.Address;
+
+                        dbContext.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+           
+    }
+}
